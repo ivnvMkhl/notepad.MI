1519887e0a838559f3790d2f2323b0bb03c0962a
ivnvMkhl <mkhl.ivnv@gamil.com>
2022-02-19 20:38:42 +0300

    commit

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Content = () => {
   return (
     <header className="mainContent">
-      <div className="noteHeader">File Edit Selection ..</div>
+      <div className="noteHeader"> Note Header</div>
       <div className="noteContent">Note content ..</div>
       <div className="noteInfo">Symbols: , Size: , ServerSync: false</div>
     </header>
